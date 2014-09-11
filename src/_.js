2ea822070b78c2c28d287ea107cf26cfd907e815
x <x@x.x>
2014-09-10 21:47:59 -0400

    no dependencies, browser support

---
+++ src/_.js
@@ -0,0 +1,34 @@
+// Uncompressed version of the _ object replacement functions
+// used in index.js
+
+function _contains(array, needle){
+  for( var i = 0, l = array.length; i < l; i++ ) {
+    if( array[i] === needle ) return true;
+  }
+  return false;
+}
+function _intersection(array) {
+  if (array == null) return [];
+  var result = [];
+  var argsLength = arguments.length;
+  for (var i = 0, length = array.length; i < length; i++) {
+    var item = array[i];
+    if (_contains(result, item)) continue;
+    for (var j = 1; j < argsLength; j++) {
+      if (!_contains(arguments[j], item)) break;
+    }
+    if (j === argsLength) result.push(item);
+  }
+  return result;
+}
+function _difference(array, bar){
+  var diff = [];
+
+  array.forEach(function(key) {
+    if (!_contains(bar, key)) {
+      diff[diff.length] = key;
+    }
+  }, this);
+
+  return diff;
+}
